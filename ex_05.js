0edc5dd9b3c4d79daa8a120c7cf7ba7f7e60df68
LUCAS875237 <lucas.renault@epitech.digital>
2024-10-02 16:02:37 +0200

    Create ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,11 @@
+function getHouses(){
+    const houses = new Map()
+  
+houses.set("apartement 75008", 90)
+houses.set("apartement 94300", 105)
+houses.set("apartement 92300", 140)
+houses.set("apartement 75015", 65)
+  return houses
+   }
+    
+console.log(getHouses())
